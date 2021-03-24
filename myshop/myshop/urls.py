71d5740bc50d4607aced855e3707181f9a401e2a
GithinjiC <codeg.iq@gmail.com>
2021-03-24 05:08:21 +0300

    Payment gateway,custom admin site

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('', include('orders.urls', namespace='orders')),
+    path('payment/', include('payment.urls', namespace='payment')),
     path('', include('shop.urls', namespace='shop')),
 ]
 if settings.DEBUG:
